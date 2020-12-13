f2d2175fd811d0d461b26794c02702c070e824ef
秋实 <qiushi95@foxmail.com>
2020-12-13 21:46:24 +0800

    update 2020-12-13

---
+++ week08/242.有效的字母异位词.go
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=golang
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+func isAnagram(s, t string) bool {
+	s1, s2 := []byte(s), []byte(t)
+	sort.Slice(s1, func(i, j int) bool { return s1[i] < s1[j] })
+	sort.Slice(s2, func(i, j int) bool { return s2[i] < s2[j] })
+	return string(s1) == string(s2)
+}
+
+// @lc code=end
+
